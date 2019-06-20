fd003d77728a21f973b09357ef249944704ccbf7
Cristiano Matte <cristianomatte@live.com>
2019-06-20 17:31:01 -0300

    Add ViewModel and Places

--- js/app.js
+++ js/app.js
@@ -1,11 +1,54 @@
 var map;
+var infoWindow;
+
+var Place = function(name, position) {
+    this.name = name;
+    this.position = position;
+    this.marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(position.lat, position.lng),
+        title: name
+    });
+
+    this.showInfo = function() {
+        this.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function(){ this.marker.setAnimation(null); }.bind(this), 500);
+
+        infoWindow.setContent("<h3>"+this.name+"</h3>");
+        infoWindow.open(map, this.marker);
+    }
+
+    this.marker.addListener('click', this.showInfo.bind(this));
+}
+
+var ViewModel = function(places) {
+    this.filter = ko.observable("");
+    this.places = ko.observableArray(places);
+    this.filteredPlaces = ko.computed(function() {
+        var filter = this.filter().toLowerCase();
+        return this.places().filter(function(place) {
+            var isVisible = place.name.toLowerCase().includes(filter);
+            place.marker.setMap(isVisible ? map : null);
+            return isVisible;
+        });
+    }, this);
+}
 
 function init() {
     var mapCanvas = $("#map").get(0);
     map = new google.maps.Map(mapCanvas, {
-        center: {lat: 40.7413549, lng: -73.9980244},
-        zoom: 13
+        center: {lat: 0, lng: 0},
+        zoom: 7
     });
+    infoWindow = new google.maps.InfoWindow({});
+
+    var places = [
+        new Place("Chapecó", {lat: 0, lng: 0}),
+        new Place("Santa Catarina", {lat: 1, lng: 0}),
+        new Place("Brasil", {lat: 2, lng: 0}),
+        new Place("América Latina", {lat: 3, lng: 0})
+    ];
+    ko.applyBindings(new ViewModel(places));
 }
 
 function initFailed() {
